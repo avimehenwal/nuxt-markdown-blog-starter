43c6256df3ee48eceb8b1cd8ddf9dcd365cc52cf
avimehenwal <avi.mehanwal@gmail.com>
2020-07-09 22:56:56 +0200

    build(genesis): :pushpin: Update generic to specific info in build files

--- pages/404.vue
+++ pages/404.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
-    Se ha producido un error
+    Sorry! This page could not be found
+    <!-- https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562_960_720.png -->
   </div>
 </template>
